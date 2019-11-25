412364dec16ae870f40904530b3668406b8cbc51
Marlon Fuentes <marlonfuenteslp@gmail.com>
2019-11-24 23:13:55 -0600

    fix Background.js

---
+++ src/Background.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+const { children } = this.props;
+export default function Background() {
+  
+  return (
+    <div className="background">{children}</div>
+  );
+}
